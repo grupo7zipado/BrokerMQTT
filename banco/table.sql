26cb73682a484fee20dae84e238c63d5d95158c9
gabriell122 <gabriell.haines@etec.sp.gov.br>
2025-03-14 16:55:55 -0300

    up testes

--- banco/table.sql
+++ banco/table.sql
@@ -32,3 +32,156 @@ CREATE TABLE dados(
     dados_generate TIMESTAMP NOT NULL,
     dados_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+
+
+
+
+
+
+
+-- --------------------------
+
+-- (SELECT dados_valor 
+--                 FROM dados 
+--                 WHERE dados.esp_id = d.esp_id AND dados_tipo = 'temperatura' 
+--                 ORDER BY dados_generate DESC LIMIT 1) AS temp_valor,
+--                 (SELECT dados_generate
+--                 FROM dados
+--                 WHERE dados.esp_id = d.esp_id AND dados_tipo = 'temperatura' 
+--                 ORDER BY dados_generate DESC LIMIT 1) AS temp_generate,
+--                 (SELECT dados_valor 
+--                 FROM dados 
+--                 WHERE dados.esp_id = d.esp_id AND dados_tipo = 'bpm' 
+--                 ORDER BY dados_generate DESC LIMIT 1) AS bpm_valor,
+--                 (SELECT dados_generate
+--                 FROM dados
+--                 WHERE dados.esp_id = d.esp_id AND dados_tipo = 'bpm' 
+--                 ORDER BY dados_generate DESC LIMIT 1) AS bpm_generate,
+--                 (SELECT dados_valor 
+--                 FROM dados 
+--                 WHERE dados.esp_id = d.esp_id AND dados_tipo = 'oxigenacao' 
+--                 ORDER BY dados_generate DESC LIMIT 1) AS oxig_valor,
+--                 (SELECT dados_generate
+--                 FROM dados
+--                 WHERE dados.esp_id = d.esp_id AND dados_tipo = 'oxigenacao' 
+--                 ORDER BY dados_generate DESC LIMIT 1) AS oxig_generate
+
+
+
+
+
+
+SELECT
+	usu.usu_id, 
+    usu_nome, 
+    usu_nascimento, 
+    (
+        SELECT esu.esp_id
+        FROM usuariosEsp  esu
+        WHERE usu.usu_id = esu.usu_id
+        ORDER BY use_id DESC
+        LIMIT 1
+ 	) AS esp_id,
+    (SELECT esu.use_id
+    FROM usuariosEsp  esu
+        WHERE usu.usu_id = esu.usu_id
+        ORDER BY use_id DESC
+        LIMIT 1
+    ) AS use_id,
+    (SELECT dados_valor 
+                FROM dados 
+                WHERE dados.use_id = d.use_id AND dados_tipo = 'temperatura' 
+                ORDER BY dados_generate DESC LIMIT 1) AS temp_valor,
+                (SELECT dados_generate
+                FROM dados
+                WHERE dados.use_id = d.use_id AND dados_tipo = 'temperatura' 
+                ORDER BY dados_generate DESC LIMIT 1) AS temp_generate,
+                (SELECT dados_valor 
+                FROM dados 
+                WHERE dados.use_id = d.use_id AND dados_tipo = 'bpm' 
+                ORDER BY dados_generate DESC LIMIT 1) AS bpm_valor,
+                (SELECT dados_generate
+                FROM dados
+                WHERE dados.use_id = d.use_id AND dados_tipo = 'bpm' 
+                ORDER BY dados_generate DESC LIMIT 1) AS bpm_generate,
+                (SELECT dados_valor 
+                FROM dados 
+                WHERE dados.use_id = d.use_id AND dados_tipo = 'oxigenacao' 
+                ORDER BY dados_generate DESC LIMIT 1) AS oxig_valor,
+                (SELECT dados_generate
+                FROM dados
+                WHERE dados.use_id = d.use_id AND dados_tipo = 'oxigenacao' 
+                ORDER BY dados_generate DESC LIMIT 1) AS oxig_generate
+FROM 
+	usuarios usu
+;
+   
+
+SELECT
+usu.usu_id, 
+usu_nome, 
+usu_nascimento, 
+(
+    SELECT esu.esp_id
+    FROM usuariosEsp  esu
+    WHERE usu.usu_id = esu.usu_id
+    ORDER BY use_id DESC
+    LIMIT 1
+) AS esp_id,
+(
+    SELECT esu.use_id
+    FROM usuariosEsp  esu
+    WHERE usu.usu_id = esu.usu_id
+    ORDER BY use_id DESC
+    LIMIT 1
+) AS use_id,
+(
+    SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = use_id AND dados_tipo = 'temperatura' 
+    ORDER BY dados_generate DESC LIMIT 1) AS temp_valor
+                
+FROM 
+	usuarios usu
+;
+   
+
+
+
+
+   INSERT INTO `dados`(`use_id`, `dados_tipo`, `dados_valor`, `dados_generate`) VALUES (11,'temperatura','321','2024-10-10')
+
+
+
+
+
+SELECT
+usu.usu_id, 
+usu_nome, 
+usu_nascimento, 
+(
+    SELECT esu.esp_id
+    FROM usuariosEsp  esu
+    WHERE usu.usu_id = esu.usu_id
+    ORDER BY use_id DESC
+    LIMIT 1
+) AS esp_id,
+(
+    SELECT esu.use_id
+    FROM usuariosEsp  esu
+    WHERE usu.usu_id = esu.usu_id
+    ORDER BY use_id DESC
+    LIMIT 1
+) AS use_id,
+(
+    SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = use_id AND dados_tipo = 'temperatura' 
+    ORDER BY dados_generate DESC LIMIT 1) AS temp_valor
+                
+FROM 
+	usuarios usu
+;
+   
