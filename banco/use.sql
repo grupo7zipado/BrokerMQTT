0f6d12c04c3581de1631212dc2e9680699140452
gabriell122 <s0mbr4h@gmail.com>
2025-03-14 21:56:07 -0300

    foi deu bom

--- banco/use.sql
+++ banco/use.sql
@@ -28,23 +28,21 @@ USE bd_tcc_des_222_sa3;
 -- 	usuarios usu
 -- ;
    
--- USE bd_tcc_des_222_sa3;  
--- SELECT
-
--- (
---     SELECT dados_valor 
---     FROM dados 
---     WHERE dados.use_id = use_id AND dados_tipo = 'temperatura' 
---     ORDER BY dados_generate DESC LIMIT 1
--- ) AS temp_valor
-                
--- FROM 
--- 	usuarios usu
--- ;
-
-    SELECT dados_valor 
+SELECT d.use_id, esu.usu_id, esp_id, usu_nome, usu_nascimento,
+    (SELECT dados_valor 
     FROM dados 
-    WHERE dados.use_id = use_id AND dados_tipo = 'temperatura' 
-    ORDER BY dados_generate DESC LIMIT
+    WHERE dados.use_id = d.use_id AND dados_tipo = 'temperatura' 
+    ORDER BY dados_generate DESC LIMIT 1) AS temp_valor,
+    (SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = d.use_id AND dados_tipo = 'bpm' 
+    ORDER BY dados_generate DESC LIMIT 1) AS bpm_valor,
+    (SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = d.use_id AND dados_tipo = 'oxigenacao' 
+    ORDER BY dados_generate DESC LIMIT 1) AS oxig_valor
 
-   
+FROM dados d
+INNER JOIN usuariosEsp esu ON esu.use_id = d.use_id
+INNER JOIN usuarios usu ON usu.usu_id = esu.usu_id
+GROUP BY use_id;
