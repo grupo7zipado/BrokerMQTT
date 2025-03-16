49033728ada4ae99fc186d01474e659d6052e30c
gabriell122 <gabriell.haines@etec.sp.gov.br>
2025-03-16 20:14:21 -0300

    up bkp

--- banco/salvar.sql
+++ banco/salvar.sql
@@ -1,75 +1,9 @@
-SELECT esp_id, 
-       (SELECT dados_valor 
-        FROM dados 
-        WHERE dados.esp_id = d.esp_id AND dados_tipo = 'temperatura' 
-        ORDER BY dados_generate DESC LIMIT 1) AS temp_valor
+
+SELECT *
 FROM dados d
-GROUP BY esp_id;
-
-
-SELECT esp_id, 
-       (SELECT dados_valor 
-        FROM dados 
-        WHERE dados.esp_id = d.esp_id AND dados_tipo = 'temperatura' 
-        ORDER BY dados_generate DESC LIMIT 1) AS temp_valor,
-		(SELECT dados_generate
-		FROM dados
-		WHERE dados.esp_id = d.esp_id AND dados_tipo = 'temperatura' 
-        ORDER BY dados_generate DESC LIMIT 1) AS temp_generate,
-		(SELECT dados_valor 
-        FROM dados 
-        WHERE dados.esp_id = d.esp_id AND dados_tipo = 'bpm' 
-        ORDER BY dados_generate DESC LIMIT 1) AS bpm_valor,
-		(SELECT dados_generate
-		FROM dados
-		WHERE dados.esp_id = d.esp_id AND dados_tipo = 'bpm' 
-        ORDER BY dados_generate DESC LIMIT 1) AS bpm_generate,
-		(SELECT dados_valor 
-        FROM dados 
-        WHERE dados.esp_id = d.esp_id AND dados_tipo = 'oxigenacao' 
-        ORDER BY dados_generate DESC LIMIT 1) AS oxig_valor,
-		(SELECT dados_generate
-		FROM dados
-		WHERE dados.esp_id = d.esp_id AND dados_tipo = 'oxigenacao' 
-        ORDER BY dados_generate DESC LIMIT 1) AS oxig_generate
-FROM dados d
-GROUP BY esp_id;
-
-
-
-SELECT 
-    d.esp_id,
-    MAX(CASE WHEN d.dados_tipo = 'temperatura' THEN d.dados_valor END) AS temp_valor,
-    MAX(CASE WHEN d.dados_tipo = 'temperatura' THEN d.dados_generate END) AS temp_generate,
-    MAX(CASE WHEN d.dados_tipo = 'bpm' THEN d.dados_valor END) AS bpm_valor,
-    MAX(CASE WHEN d.dados_tipo = 'bpm' THEN d.dados_generate END) AS bpm_generate,
-    MAX(CASE WHEN d.dados_tipo = 'oxigenacao' THEN d.dados_valor END) AS oxig_valor,
-    MAX(CASE WHEN d.dados_tipo = 'oxigenacao' THEN d.dados_generate END) AS oxig_generate
-FROM dados d
-WHERE (d.esp_id, d.dados_generate) IN (
-    SELECT esp_id, MAX(dados_generate) 
-    FROM dados 
-    GROUP BY esp_id, dados_tipo
-)
-GROUP BY d.esp_id;
-
-
-
-
-	1	355
-	2	355
-	3	356
-	4	340
-	5	355
-	6	359
-	7	343
-	8	341
-	9	356
-	10	341
-	11	342
-	12	347
-	13	358
-	14	350
-	15	341
-	16	342
-	17	355
+     INNER JOIN (SELECT esu.usu_id, MAX(d2.dados_id) dados_id
+                 FROM usuariosEsp esu
+                     INNER JOIN dados d2
+                     ON d2.use_id = esu.use_id
+                 GROUP BY esu.usu_id, d2.dados_tipo) t
+     ON d.dados_id = t.dados_id
--- banco/use.sql
+++ banco/use.sql
@@ -1,33 +1,4 @@
 USE bd_tcc_des_222_sa3;  
--- SELECT
--- usu.usu_id, 
--- usu_nome, 
--- usu_nascimento, 
--- (
---     SELECT esu.esp_id
---     FROM usuariosEsp  esu
---     WHERE usu.usu_id = esu.usu_id
---     ORDER BY use_id DESC
---     LIMIT 1
--- ) AS esp_id,
--- (
---     SELECT esu.use_id
---     FROM usuariosEsp  esu
---     WHERE usu.usu_id = esu.usu_id
---     ORDER BY use_id DESC
---     LIMIT 1
--- ) AS use_id,
--- (
---     SELECT dados_valor 
---     FROM dados 
---     WHERE dados.use_id = use_id AND dados_tipo = 'temperatura' 
---     ORDER BY dados_generate DESC LIMIT 1
--- ) AS temp_valor
-                
--- FROM 
--- 	usuarios usu
--- ;
-   
 SELECT d.use_id, esu.usu_id, esp_id, usu_nome, usu_nascimento,
     (SELECT dados_valor 
     FROM dados 
@@ -46,3 +17,154 @@ FROM dados d
 INNER JOIN usuariosEsp esu ON esu.use_id = d.use_id
 INNER JOIN usuarios usu ON usu.usu_id = esu.usu_id
 GROUP BY use_id;
+
+
+
+SELECT d.use_id, esu.usu_id, esp_id, usu_nome, usu_nascimento,
+    (SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = d.use_id AND dados_tipo = 'temperatura' 
+    ORDER BY dados_generate DESC LIMIT 1) AS temp_valor,
+    (SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = d.use_id AND dados_tipo = 'bpm' 
+    ORDER BY dados_generate DESC LIMIT 1) AS bpm_valor,
+    (SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = d.use_id AND dados_tipo = 'oxigenacao' 
+    ORDER BY dados_generate DESC LIMIT 1) AS oxig_valor
+FROM dados d
+INNER JOIN usuariosEsp esu ON esu.use_id = d.use_id 
+INNER JOIN usuarios usu ON usu.usu_id = esu.usu_id
+GROUP BY usu_id;
+
+
+SELECT das.use_id, esu.usu_id, esp_id, usu_nome, usu_nascimento
+FROM usuariosEsp esu
+INNER JOIN usuarios usu ON usu.usu_id = esu.usu_id
+GROUP BY usu_id DESC
+
+
+
+SELECT esu.use_id, esu.usu_id, esu.esp_id, usu.usu_nome, usu.usu_nascimento,
+    (SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = d.use_id AND dados_tipo = 'temperatura' 
+    ORDER BY dados_generate DESC LIMIT 1) AS temp_valor,
+    (SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = d.use_id AND dados_tipo = 'bpm' 
+    ORDER BY dados_generate DESC LIMIT 1) AS bpm_valor,
+    (SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = d.use_id AND dados_tipo = 'oxigenacao' 
+    ORDER BY dados_generate DESC LIMIT 1) AS oxig_valor
+FROM usuariosEsp esu
+INNER JOIN usuarios usu ON usu.usu_id = esu.usu_id
+INNER JOIN dados d ON d.use_id = esu.use_id
+WHERE esu.use_id = (
+    SELECT MAX(use_id) 
+    FROM usuariosEsp 
+    WHERE usu_id = esu.usu_id
+)
+ORDER BY esu.use_id DESC;
+
+
+
+
+
+
+
+
+
+-----------------------------------------------------------------------
+
+-- VALIDADO MAS BUGADO
+SELECT esu.use_id, esu.usu_id, esu.esp_id, usu.usu_nome, usu.usu_nascimento,
+    (SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = d.use_id AND dados_tipo = 'temperatura' 
+    ORDER BY dados_generate DESC LIMIT 1) AS temp_valor,
+    (SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = d.use_id AND dados_tipo = 'bpm' 
+    ORDER BY dados_generate DESC LIMIT 1) AS bpm_valor,
+    (SELECT dados_valor 
+    FROM dados 
+    WHERE dados.use_id = d.use_id AND dados_tipo = 'oxigenacao' 
+    ORDER BY dados_generate DESC LIMIT 1) AS oxig_valor
+FROM usuariosEsp esu
+INNER JOIN usuarios usu ON usu.usu_id = esu.usu_id
+INNER JOIN dados d ON d.use_id = esu.use_id
+WHERE esu.use_id = (
+    SELECT MAX(use_id) 
+    FROM usuariosEsp t
+    WHERE t.usu_id = esu.usu_id
+)
+GROUP BY usu.usu_id;
+
+
+SELECT *
+FROM dados d
+     INNER JOIN (SELECT esu.usu_id, MAX(d2.dados_id) dados_id
+                 FROM usuariosEsp esu
+                     INNER JOIN dados d2
+                     ON d2.use_id = esu.use_id
+                 GROUP BY esu.usu_id, d2.dados_tipo) t
+     ON d.dados_id = t.dados_id
+;
+
+
+
+SELECT TOP 1 esu.use_id-- , esu.usu_id, esu.esp_id, usu.usu_nome, usu.usu_nascimento,
+    -- (SELECT dados_valor 
+    -- FROM dados 
+    -- WHERE dados.use_id = d.use_id AND dados_tipo = 'temperatura' 
+    -- ORDER BY dados_generate DESC LIMIT 1) AS temp_valor,
+    -- (SELECT dados_valor 
+    -- FROM dados 
+    -- WHERE dados.use_id = d.use_id AND dados_tipo = 'bpm' 
+    -- ORDER BY dados_generate DESC LIMIT 1) AS bpm_valor,
+    -- (SELECT dados_valor 
+    -- FROM dados 
+    -- WHERE dados.use_id = d.use_id AND dados_tipo = 'oxigenacao' 
+    -- ORDER BY dados_generate DESC LIMIT 1) AS oxig_valor
+FROM usuariosEsp esu
+-- INNER JOIN usuarios usu ON usu.usu_id = esu.usu_id
+-- INNER JOIN dados d ON d.use_id = esu.use_id
+-- WHERE esu.use_id = (
+--     SELECT MAX(use_id) 
+--     FROM usuariosEsp t
+--     -- WHERE t.usu_id = esu.usu_id
+-- )
+ORDER BY esu.use_id DESC
+-- GROUP BY usu.usu_id;
+
+-- SELECT *
+-- FROM usuarios usu,
+--      LATERAL (SELECT *
+--               FROM usuariosEsp esu
+--                    INNER JOIN dados d
+--                    ON d.use_id = esu.use_id
+--               WHERE esu.usu_id = usu.usu_id
+--               ORDER BY esu.use_id DESC
+--               LIMIT 1) AS t
+
+-- SELECT *
+--       ,(SELECT *
+--         FROM usuariosEsp esu
+--              INNER JOIN dados d
+--              ON d.use_id = esu.use_id
+--         WHERE esu.usu_id = usu.usu_id
+--         ORDER BY esu.use_id DESC
+--         LIMIT 1)
+-- FROM usuarios usu
+
+
+-- SELECT esu.usu_id, MAX(d2.use_id)
+-- FROM usuariosEsp esu
+--      INNER JOIN dados d2
+--      ON d2.use_id = esu.use_id
+-- GROUP BY esu.usu_id
+
+
